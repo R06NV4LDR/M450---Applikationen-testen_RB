fcf4e7d046aba11d4a03098734eb977fe268454d
Ronny A. Bruhin <ronny.bruhin+github@hotmail.com>
2025-09-12 10:35:30 +0200

    fourth day | implemented AddressComparator correctly | checks for last-, firstname and phonenumber and compares them

--- src/main/java/ch/roones/uebungen/_04fourthday/util/AddressComparator.java
+++ src/main/java/ch/roones/uebungen/_04fourthday/util/AddressComparator.java
@@ -6,10 +6,18 @@ import java.util.Comparator;
 
 public class AddressComparator implements Comparator<Address> {
 
+    private static final Comparator<String> STRING_COMPARATOR = Comparator.nullsLast(String.CASE_INSENSITIVE_ORDER);
+
     @Override
     public int compare(Address a1, Address a2) {
+        if (a1 == a2) return 0;
+        if (a1 == null) return 1;
+        if (a2 == null) return -1;
         // Wrong implementation, please change me
-        return -1;
+        return Comparator.comparing(Address::getLastname, STRING_COMPARATOR)
+                .thenComparing(Address::getFirstname, STRING_COMPARATOR)
+                .thenComparing(Address::getPhonenumber, STRING_COMPARATOR)
+                .compare(a1, a2);
     }
 
 }
